bd9e71821835bffbc936489d1e93b2f20f2eb101
yuppe19 <yuppe19@live.co.uk>
2016-02-23 18:13:54 +0900

    処理が遅いので、進捗状況を出すようにしました

--- tcjapan.py
+++ tcjapan.py
@@ -11,6 +11,8 @@ N = 200
 sett = set(line.strip() for line in sys.stdin.readlines())
 
 for i in range(3):
+    sys.stderr.write('{}ページめをやります ... '.format(i+1))
+    sys.stderr.flush()
     url = 'http://community.topcoder.com/tc?cc=392&sc=&sd=&cc=392&module=AlgoRank&nr={}&sr={}'.format(N, i*N+1)
     res = requests.get(url)
     if res.status_code != 200:
@@ -21,6 +23,8 @@ for i in range(3):
     ele = doc.xpath("//table[contains(@class, 'stat')]/tbody/tr/td[2]/a")
     for e in ele:
         sett.add(e.text)
+    sys.stderr.write('完了\n')
+    sys.stderr.flush()
     sleep(1.1)
 
 print('\n'.join(sorted(sett)))
