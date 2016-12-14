5376c07cb6a9c7a3fd7a646089ab6a01b7b5b96f
yuppe19 <yuppe19@live.co.uk>
2016-12-14 15:54:46 +0900

    やっぱ Python2 にしました

--- tcjapan.py
+++ tcjapan.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python2
 # -*- coding: utf-8 -*-
 # †
 import sys
@@ -10,13 +10,13 @@ N = 200
 
 sett = set(line.strip() for line in sys.stdin.readlines())
 
-for i in range(3):
+for i in xrange(3):
     sys.stderr.write('{}ページめをやります ... '.format(i+1))
     sys.stderr.flush()
     url = 'http://community.topcoder.com/tc?cc=392&sc=&sd=&cc=392&module=AlgoRank&nr={}&sr={}'.format(N, i*N+1)
     res = requests.get(url)
     if res.status_code != 200:
-        print('(ΦωΦ)＜not 200', file=sys.stderr)
+        sys.stderr.write('(ΦωΦ)＜not 200\n')
         exit(1)
     res0 = res.content.decode('utf-8')
     doc = lxml.html.fromstring(res0)
@@ -27,4 +27,4 @@ for i in range(3):
     sys.stderr.flush()
     sleep(1.1)
 
-print('\n'.join(sorted(sett)))
+print '\n'.join(sorted(sett))
