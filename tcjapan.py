96699a7e3bc17ddd3631258ca0b2064be377c581
yuppe19 <yuppe19@live.co.uk>
2018-10-01 08:29:07 +0900

    SRM 738

--- tcjapan.py
+++ tcjapan.py
@@ -6,26 +6,38 @@ import requests
 import lxml.html
 from time import sleep
 
+def nya(msg):
+    sys.stderr.write(msg)
+    sys.stderr.flush()
+
 N = 200
 
-sett = set(line.strip() for line in sys.stdin.readlines())
+players = set()
 
 for i in xrange(3):
     if i:
         sleep(1.1)
-    sys.stderr.write('{}ページめをやります ... '.format(i+1))
-    sys.stderr.flush()
-    url = 'http://community.topcoder.com/tc?cc=392&sc=&sd=&cc=392&module=AlgoRank&nr={}&sr={}'.format(N, i*N+1)
+    nya('{}ページめをやります ... '.format(i+1))
+    url = 'https://community.topcoder.com/tc?cc=392&sc=&sd=&cc=392&module=AlgoRank&nr={}&sr={}'.format(N, i*N+1)
     res = requests.get(url)
     if res.status_code != 200:
-        sys.stderr.write('(ΦωΦ)＜not 200\n')
+        nya('(ΦωΦ)＜not 200\n')
         exit(1)
     res0 = res.content.decode('utf-8')
     doc = lxml.html.fromstring(res0)
     ele = doc.xpath("//table[contains(@class, 'stat')]/tbody/tr/td[2]/a")
     for e in ele:
-        sett.add(e.text)
-    sys.stderr.write('完了\n')
-    sys.stderr.flush()
+        players.add(e.text)
+    nya('完了\n')
 
-print '\n'.join(sorted(sett))
+with open('japan', 'r') as fleobj:
+    for line in fleobj.readlines():
+        players.add(line.rstrip())
+
+players = sorted(list(players))
+
+with open('japan', 'w') as fleobj:
+    for p in players:
+        fleobj.write(p + '\n')
+
+nya('(ΦωΦ)＜gitにコミット->プッシュを忘れずに！\n')
